f5faeb80889ad195c44d9eeed16e9ead4bdcf34e
Aryan Jain <109624558+aryanj112@users.noreply.github.com>
2026-07-14 21:19:22 -0700

    Add: valid-sudoku - submission-6

---
+++ Data Structures & Algorithms/valid-sudoku/submission-6.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        # check rows
+        for row in range(9):
+            hashset = set()
+            for col in range(9):
+                if board[row][col] in hashset and board[row][col] != ".":
+                    return False
+                hashset.add(board[row][col])
+        print("ROWS ARE FINE")        
+
+        # check cols
+        for col in range(9):
+            hashset = set()
+            for row in range(9):
+                if board[row][col] in hashset and board[row][col] != ".":
+                    return False
+                hashset.add(board[row][col])
+        print("COLS ARE FINE")        
+
+        def check_box(row,col):
+            # given a row and col coordinate check a box around it
+            hashset = set()
+            for i in range(3):
+                for j in range(3):
+                    if board[row+i][col+j] in hashset and board[row+i][col+j] != ".":
+                        return False
+                    hashset.add(board[row+i][col+j])
+            return True   
+
+        # check boxes
+        for row in [0,3,6]:
+            for col in [0,3,6]:
+                if not check_box(row,col):
+                    return False       
+        return True
+        
