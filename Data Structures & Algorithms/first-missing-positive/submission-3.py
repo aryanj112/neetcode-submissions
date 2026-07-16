206568bfeaf8c5018eb1246adc28e8ff85d7c362
Aryan Jain <109624558+aryanj112@users.noreply.github.com>
2026-07-16 12:51:20 -0700

    Add: first-missing-positive - submission-3

---
+++ Data Structures & Algorithms/first-missing-positive/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        print(nums)
+        for i, n in enumerate(nums):
+            if n < 0:
+                nums[i] = 0
+        print(nums)
+        res = 1
+
+        def in_bounds(i):
+            if i >= 0 and i < len(nums):
+                return True
+            return False
+
+        for i, n in enumerate(nums):
+            
+            idx = abs(n) - 1
+            if in_bounds(idx):
+                num = -abs(nums[idx])
+                if num == 0:
+                    num = -1
+                nums[idx] = num
+            
+        print(nums)
+        # [-2, -1, 0]
+        # [0, 0, 0]
+        # [0, 0, 0]
+
+        # if we have a length of 3 the max number our missing positive could be is len(nums) + 1
+        # start at i = 1 and have res go through it and just check does that number exist super cool
+        for i in range(1, len(nums) + 1):
+            if nums[i - 1] >= 0:
+                return i
+        return len(nums) + 1
